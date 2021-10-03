b08ddf95b80eb3a1ab8ca9f7d883a540a112b533
Hasan H. Uğur <hasan.ugur4535@gmail.com>
2021-10-03 15:43:23 +0300

    Change api domain address

--- src/services/services.js
+++ src/services/services.js
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const BASE_API = "https://baconipsum.com/";
 export const getParagraphsAsync = createAsyncThunk(
   "paragraphs/getParagraphsAsync",
   async (data) => {
     const res = await axios.post(
-      `${process.env.REACT_APP_API_BASE_ENDPOINT}api?type=all-meat&paras=${data.paragraphCount}&start-with-lorem=1&format=${data.includeHtml}`
+      `${BASE_API}api?type=all-meat&paras=${data.paragraphCount}&start-with-lorem=1&format=${data.includeHtml}`
     );
 
     return res.data.split("\n");
